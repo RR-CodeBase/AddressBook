79e9c8158d6cd4616b9e456798e9101afe64baf3
Ruan Roos <RuanRoos@flowise.co.za>
2020-10-28 01:05:46 +0200

    Add project files.

---
+++ Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AddressBook.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public int ContactID { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
